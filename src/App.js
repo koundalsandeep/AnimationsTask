8e3bb0cd0e908e315460a2a6639a1b7e667c89ed
koundalsandeep <koundalsandeep@gmail.com>
2024-08-21 16:57:58 +0530

    first 2 animations

--- src/App.js
+++ src/App.js
@@ -61,20 +61,6 @@ function App() {
           onOptionChange={(option, value) => handleOptionChange(effect, option, value)}
         />
       ))}
-      <div className="button-container">
-        <h3>Duration</h3>
-        <div className="duration-buttons">
-          {['1s', '2s', '3s', '4s', '5s'].map(duration => (
-            <button
-              key={duration}
-              className='button'
-              onClick={() => document.documentElement.style.setProperty('--animation-duration', duration)}
-            >
-              {duration}
-            </button>
-          ))}
-        </div>
-      </div>
     </div>
   );
 }
@@ -92,6 +78,21 @@ function CarouselSlider({ effect, slides, options, onOptionChange }) {
     setActiveIndex(prevIndex);
   };
 
+  const getFloatDirectionStyle = () => {
+    if (!options.direction) return {};
+    switch (options.direction.toLowerCase()) {
+      case 'top':
+        return { transform: 'translateY(-100%)' };
+      case 'bottom':
+        return { transform: 'translateY(100%)' };
+      case 'left':
+        return { transform: 'translateX(-100%)' };
+      case 'right':
+        return { transform: 'translateX(100%)' };
+      default:
+        return {};
+    }
+  };
   return (
     <div className={`carousel-container ${effect}`}>
       <h2>{effect.charAt(0).toUpperCase() + effect.slice(1)} Animation</h2>
@@ -121,7 +122,7 @@ function CarouselSlider({ effect, slides, options, onOptionChange }) {
             >
               <option value='top'>Top</option>
               <option value='bottom'>Bottom</option>
-              <option value='lrft'>Left</option>
+              <option value='left'>Left</option>
               <option value='right'>Right</option>
             </select>
             <label>Duration:</label>
@@ -138,312 +139,20 @@ function CarouselSlider({ effect, slides, options, onOptionChange }) {
             />
           </>
         )}
-        {effect === 'blur' && (
-          <>
-            <label>Motion Style:</label>
-            <select
-              value={options.style}
-              onChange={e => onOptionChange('style', e.target.value)}
-            >
-              <option>Gentle</option>
-              <option>Moderate</option>
-              <option>Intense</option>
-            </select>
-            <label>Duration:</label>
-            <input
-              type="text"
-              value={options.duration}
-              onChange={e => onOptionChange('duration', e.target.value)}
-            />
-            <label>Delay:</label>
-            <input
-              type="text"
-              value={options.delay}
-              onChange={e => onOptionChange('delay', e.target.value)}
-            />
-          </>
-        )}
-        {effect === 'expand' && (
-          <>
-            <label>Motion Style:</label>
-            <select
-              value={options.style}
-              onChange={e => onOptionChange('style', e.target.value)}
-            >
-              <option>Gentle</option>
-              <option>Moderate</option>
-              <option>Intense</option>
-            </select>
-            <label>Direction:</label>
-            <select
-              value={options.direction}
-              onChange={e => onOptionChange('direction', e.target.value)}
-            >
-              <option>Right</option>
-              <option>Top Right</option>
-              <option>Top</option>
-              <option>Top Left</option>
-              <option>Left</option>
-              <option>Bottom Left</option>
-              <option>Bottom</option>
-              <option>Bottom Right</option>
-              <option>Center</option>
-            </select>
-            <label>Duration:</label>
-            <input
-              type="text"
-              value={options.duration}
-              onChange={e => onOptionChange('duration', e.target.value)}
-            />
-            <label>Delay:</label>
-            <input
-              type="text"
-              value={options.delay}
-              onChange={e => onOptionChange('delay', e.target.value)}
-            />
-          </>
-        )}
-        {effect === 'shrink' && (
-          <>
-            <label>Motion Style:</label>
-            <select
-              value={options.style}
-              onChange={e => onOptionChange('style', e.target.value)}
-            >
-              <option>Gentle</option>
-              <option>Moderate</option>
-              <option>Intense</option>
-            </select>
-            <label>Duration:</label>
-            <input
-              type="text"
-              value={options.duration}
-              onChange={e => onOptionChange('duration', e.target.value)}
-            />
-            <label>Delay:</label>
-            <input
-              type="text"
-              value={options.delay}
-              onChange={e => onOptionChange('delay', e.target.value)}
-            />
-          </>
-        )}
-        {effect === 'reveal' && (
-          <>
-            <label>Direction:</label>
-            <select
-              value={options.direction}
-              onChange={e => onOptionChange('direction', e.target.value)}
-            >
-              <option>Top</option>
-              <option>Bottom</option>
-              <option>Left</option>
-              <option>Right</option>
-            </select>
-            <label>Duration:</label>
-            <input
-              type="text"
-              value={options.duration}
-              onChange={e => onOptionChange('duration', e.target.value)}
-            />
-            <label>Delay:</label>
-            <input
-              type="text"
-              value={options.delay}
-              onChange={e => onOptionChange('delay', e.target.value)}
-            />
-          </>
-        )}
-        {effect === 'shape' && (
-          <>
-            <label>Shape:</label>
-            <select
-              value={options.shape}
-              onChange={e => onOptionChange('shape', e.target.value)}
-            >
-              <option>Oval</option>
-              <option>Circle</option>
-              <option>Square</option>
-              <option>Diamond</option>
-            </select>
-            <label>Duration:</label>
-            <input
-              type="text"
-              value={options.duration}
-              onChange={e => onOptionChange('duration', e.target.value)}
-            />
-            <label>Delay:</label>
-            <input
-              type="text"
-              value={options.delay}
-              onChange={e => onOptionChange('delay', e.target.value)}
-            />
-          </>
-        )}
-        {effect === 'flip' && (
-          <>
-            <label>Motion Style:</label>
-            <select
-              value={options.style}
-              onChange={e => onOptionChange('style', e.target.value)}
-            >
-              <option>Gentle</option>
-              <option>Moderate</option>
-              <option>Intense</option>
-            </select>
-            <label>Direction:</label>
-            <select
-              value={options.direction}
-              onChange={e => onOptionChange('direction', e.target.value)}
-            >
-              <option>Top</option>
-              <option>Bottom</option>
-              <option>Left</option>
-              <option>Right</option>
-            </select>
-            <label>Duration:</label>
-            <input
-              type="text"
-              value={options.duration}
-              onChange={e => onOptionChange('duration', e.target.value)}
-            />
-            <label>Delay:</label>
-            <input
-              type="text"
-              value={options.delay}
-              onChange={e => onOptionChange('delay', e.target.value)}
-            />
-          </>
-        )}
-        {effect === 'grow' && (
-          <>
-            <label>Motion Style:</label>
-            <select
-              value={options.style}
-              onChange={e => onOptionChange('style', e.target.value)}
-            >
-              <option>Gentle</option>
-              <option>Moderate</option>
-              <option>Intense</option>
-            </select>
-            <label>Direction (0-360°):</label>
-            <input
-              type="text"
-              value={options.direction}
-              onChange={e => onOptionChange('direction', e.target.value)}
-            />
-            <label>Distance:</label>
-            <input
-              type="text"
-              value={options.distance}
-              onChange={e => onOptionChange('distance', e.target.value)}
-            />
-            <label>Duration:</label>
-            <input
-              type="text"
-              value={options.duration}
-              onChange={e => onOptionChange('duration', e.target.value)}
-            />
-            <label>Delay:</label>
-            <input
-              type="text"
-              value={options.delay}
-              onChange={e => onOptionChange('delay', e.target.value)}
-            />
-          </>
-        )}
-        {effect === 'wink' && (
-          <>
-            <label>Direction:</label>
-            <select
-              value={options.direction}
-              onChange={e => onOptionChange('direction', e.target.value)}
-            >
-              <option>Horizontal</option>
-              <option>Vertical</option>
-            </select>
-            <label>Duration:</label>
-            <input
-              type="text"
-              value={options.duration}
-              onChange={e => onOptionChange('duration', e.target.value)}
-            />
-            <label>Delay:</label>
-            <input
-              type="text"
-              value={options.delay}
-              onChange={e => onOptionChange('delay', e.target.value)}
-            />
-          </>
-        )}
-        {effect === 'slide' && (
-          <>
-            <label>Motion Style:</label>
-            <select
-              value={options.style}
-              onChange={e => onOptionChange('style', e.target.value)}
-            >
-              <option>Gentle</option>
-              <option>Moderate</option>
-              <option>Intense</option>
-            </select>
-            <label>Direction:</label>
-            <select
-              value={options.direction}
-              onChange={e => onOptionChange('direction', e.target.value)}
-            >
-              <option>Top</option>
-              <option>Bottom</option>
-              <option>Left</option>
-              <option>Right</option>
-            </select>
-            <label>Duration:</label>
-            <input
-              type="text"
-              value={options.duration}
-              onChange={e => onOptionChange('duration', e.target.value)}
-            />
-            <label>Delay:</label>
-            <input
-              type="text"
-              value={options.delay}
-              onChange={e => onOptionChange('delay', e.target.value)}
-            />
-          </>
-        )}
-        {effect === 'flash' && (
-          <>
-            <label>Duration:</label>
-            <input
-              type="text"
-              value={options.duration}
-              onChange={e => onOptionChange('duration', e.target.value)}
-            />
-            <label>Delay Between Loops:</label>
-            <input
-              type="text"
-              value={options.delay}
-              onChange={e => onOptionChange('delay', e.target.value)}
-            />
-          </>
-        )}
+
       </div>
       <div className="carousel slide carouselWidth">
         <div className="carousel-inner">
           {slides.map((slide, slideIndex) => (
             <div
-              key={slideIndex}
-              className={`carousel-item ${slideIndex === activeIndex ? 'active' : ''} ${effect}`}
-              style={{ 
-                '--animation-duration': options.duration,
-                '--animation-delay': options.delay,
-                '--animation-direction': options.direction,
-                '--animation-distance': options.distance,
-                '--animation-shape': options.shape,
-                '--animation-style': options.style,
-                // Additional dynamic styles for other options
-              }}
-            >
+            key={slideIndex}
+            className={`carousel-item ${slideIndex === activeIndex ? 'active' : ''} ${effect}`}
+            style={{
+              ...getFloatDirectionStyle(),
+              '--animation-duration': options.duration,
+              '--animation-delay': options.delay,
+            }}
+          >
               <img src={slide} className="d-block w-100" alt={`Slide ${slideIndex + 1}`} />
             </div>
           ))}
